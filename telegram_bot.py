3bf16eb92ef57285dca6b3ae69a895a2171eed97
Jiho Shin <ziho@kakao.com>
2020-09-16 20:40:59 +0900

    Update telegram_bot.py
    Delete token,id

--- telegram_bot.py
+++ telegram_bot.py
@@ -12,8 +12,8 @@ class TelegramBot:
 
 
 if __name__ == "__main__":
-    token = '1167349229:AAHFdxH4pEDCBFtsGTtngjiD7fDSfaluiBA'  # your token
+    token = ''  # your token
     bot = TelegramBot(token)
-    receiver_id = '1083114017'  # your id
+    receiver_id = ''  # your id
 
     bot.sendMessage(receiver_id, "Start")
